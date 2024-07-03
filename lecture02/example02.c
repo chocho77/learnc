084a0fe43c18a3fb9f13d1104a548049dcc5443a
chocho77 <chavmom@gmail.com>
2024-07-03 08:57:57 +0300

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	new file:   lecture01/HelloWorld.c 	new file:   lecture02/a 	new file:   lecture02/a-one 	new file:   lecture02/example01.c 	new file:   lecture02/example02.c

---
+++ lecture02/example02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int myAge = 43; // an int variable
+
+    printf("%d\n", myAge);  // Outputs the value of myAge (43)
+    printf("%p\n", &myAge); // Outputs the memory address of myAge (0x7fff69baac4c)
+
+    return 0;
+
+}

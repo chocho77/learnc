0e2d01e307bd11107efed0f9a7c1f1fa9f8195b5
chocho77 <chavmom@gmail.com>
2024-07-03 11:28:36 +0300

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	new file:   lecture01/OutputData.c 	new file:   lecture01/a

---
+++ lecture01/OutputData.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// https://www.w3schools.com/c/c_output.php
+
+int main() {
+  printf("Hello World!");
+  printf("I am learning C.");
+  printf("And it is awesome.\n");
+  return 0;
+}
